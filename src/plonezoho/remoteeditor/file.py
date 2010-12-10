c0f5123d2da52c74320632311284e99751920bc2
Rok Garbas <rok@garbas.si>
2010-12-10 15:22:20 +0000

    adding annotation support to store documentid

--- src/plonezoho/remoteeditor/file.py
+++ src/plonezoho/remoteeditor/file.py
@@ -5,20 +5,51 @@ import zohoapi
 import zope.component
 import plone.registry
 import plone.memoize.view
+import zope.annotation
 
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+
+PLONEZOHO_REMOTEEDITOR = 'plonezoho.remoteeditor'
 
 
 class RemoteBase(BrowserView):
+    """
+    """
+
+    DOCID = PLONEZOHO_REMOTEEDITOR+ '-file'
 
     def __init__(self, context, request):
         super(RemoteBase, self).__init__(context, request)
         registry = zope.component.getUtility(plone.registry.interfaces.IRegistry)
+        apikey = registry.get('plonezoho.remoteapi.apikey')
+        skey = registry.get('plonezoho.remoteapi.skey')
+        if not skey:
+            skey = None
         self.remoteapi = zohoapi.Remote(
-                registry.get('plonezoho.remoteapi.apikey'),
-                registry.get('plonezoho.remoteapi.skey'),
+                apikey=apikey,
+                saveurl='%s/@@remote-save?id=%s' % (
+                    self.context.absolute_url(),
+                    self.context.UID(),
+                    ),
+                skey=skey,
                 )
+        self.annotations = zope.annotation.interfaces.IAnnotations(self.context)
+        self.membership = getToolByName(self.context, 'portal_membership')
+
+    @property
+    def user(self):
+        if self.membership.isAnonymousUser(): # the user has not logged in
+            return None
+        else:
+            member = self.membership.getAuthenticatedMember()
+            fullname = member.getProperty('fullname', None)
+            username = member.getUserName()
+            if fullname:
+                return fullname + ' (' + username + ')'
+            return username
 
     @property
     def language(self):
@@ -28,11 +59,15 @@ class RemoteBase(BrowserView):
         portal_state = self.context.unrestrictedTraverse("@@plone_portal_state")
         return portal_state.default_language()
 
-    @property
-    def documentid(self):
-        return 'e2e696359ed3e3bda266dfd07ad3fbccdebf17855dc5f624'
-        # TODO: should also look at annotation
-        return hashlib.sha224(self.context.UID() + self.remoteapi.apikey).hexdigest()
+    def get_docid(self):
+        if self.DOCID in self.annotations.keys():
+            docid = self.annotations[self.DOCID]
+            if docid is not None:
+                return docid
+        return self.context.UID()
+    def set_docid(self, value):
+        self.annotations[self.DOCID] = value
+    documentid = property(get_docid, set_docid)
 
     @property
     def format(self):
@@ -44,12 +79,21 @@ class RemoteBase(BrowserView):
 
     @property
     def status(self):
+        if self.documentid is None:
+            return None
         return self._status()
 
     @plone.memoize.view.memoize
     def _status(self):
         status = self.remoteapi.status(self.doctype, self.documentid)
-        #import ipdb; ipdb.set_trace()
+        # remove documentid from annotation only if:
+        #  - status request was OK,
+        #   - nobody is working on document and
+        #   - documentid alredady exists
+        if status.get('result', True) and \
+           status.get('collaboratorsCount', 0) == 0 and \
+           self.documentid is not None:
+            del self.annotations[self.DOCID]
         return status
 
 
@@ -63,15 +107,17 @@ class RemoteEditor(RemoteBase):
         file_ = self.context.getFile()
         blob_ = file_.getBlob().open()
         response = self.remoteapi.collab_edit(
-                documentid=self.documentid,
                 filename=file_.filename,
                 content=blob_,
                 format=self.format,
                 lang=self.language,
+                username=self.user,
+                documentid=self.documentid,
                 )
         import ipdb; ipdb.set_trace()
         self.request.response.redirect(response.url)
         blob_.close()
+
 
 class RemoteSave(RemoteBase):
 
